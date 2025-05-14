617a2a3606bd57950afb030f89863a59ded07f22
averelli <beloglazov.roman06@gmail.com>
2025-05-14 14:55:56 +0300

    chore: add initial mongo interactions

---
+++ db/mongo.py
@@ -0,0 +1,25 @@
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
+from config import settings
+
+class MongoDBClient:
+    def __init__(self, db_name: str):
+        self.client = MongoClient(settings.MONGO_URI)
+        self.db = self.client[db_name]
+
+    def get_collection(self, collection_name):
+        return self.db[collection_name]
+    
+    def insert_post(self, data:dict, collection_name:str):
+        collection = self.db[collection_name]
+        try:
+            collection.insert_one(data)
+        except DuplicateKeyError:
+            print("Duplicate key detected")
+
+    def close(self):
+        self.client.close()
+
+m = MongoDBClient("testing")
+m.get_collection("testing_col")
+m.insert_post({"test": 1}, "testing_col")
