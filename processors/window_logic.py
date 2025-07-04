fb9f49c32072c48cc1320581e2d701dbcea50863
averelli <beloglazov.roman06@gmail.com>
2025-07-04 13:25:05 +0300

    fix: fixed major bugs in windowing logic, add logging

--- processors/window_logic.py
+++ processors/window_logic.py
@@ -1,12 +1,14 @@
 from collections import  Counter, deque
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from logging import Logger
 from data import COUNTRIES
 
 class WindowAggregator:
-    def __init__(self, window_size_seconds=60):
+    def __init__(self, logger: Logger, event_label:str, window_size_seconds=60):
+        self.logger = logger
         self.WINDOW_SIZE = timedelta(seconds=window_size_seconds)
         self.ROLLING_WINDOW = deque()
+        self.event_label = event_label
         self.total_stats = {
             "total_posts": 0,
             "total_sentiment": 0.0,
@@ -17,11 +19,13 @@ class WindowAggregator:
         }
 
     def prune_window(self):
-        now = datetime.now()
+        self.logger.info(f"Prunning the window")
+        now = datetime.now(timezone.utc)
         while self.ROLLING_WINDOW and now - self.ROLLING_WINDOW[0][0] > self.WINDOW_SIZE:
             self.ROLLING_WINDOW.popleft()
 
     def aggregate_window(self):
+        self.logger.info("Aggregating the window")
         window_sentiment = 0.0
         window_posts = len(self.ROLLING_WINDOW)
         platform_counter = Counter()
@@ -45,24 +49,22 @@ class WindowAggregator:
         # add to the total counters
         self.total_stats["total_posts"] += window_posts
         self.total_stats["total_sentiment"] += window_sentiment
-        self.total_stats["platform_counter"] += platform_counter
-        self.total_stats["total_sentiment"] += window_sentiment
-        self.total_stats["platform_counter"] += platform_counter
+        self.total_stats["total_platform_counter"] += platform_counter
         self.total_stats["total_contestant_counter"] += contestant_counter
         self.total_stats["total_country_counter"] += country_counter
         self.total_stats["total_song_counter"] += song_counter
 
-        # get the top values
-        window_top_song = song_counter.most_common(1)[0] #("song": count) type
-        total_top_song = song_counter.most_common(1)[0]
-        window_top_country = country_counter.most_common(1)[0]
-        total_top_country = country_counter.most_common(1)[0]
-        window_top_contestant = contestant_counter.most_common(1)[0]
-        total_top_contestant = contestant_counter.most_common(1)[0]
+        # get the top values or None if empty
+        window_top_song = song_counter.most_common(1)[0] if song_counter else None #("song": count) type
+        total_top_song = self.total_stats["total_song_counter"].most_common(1)[0] if self.total_stats["total_song_counter"] else None
+        window_top_country = country_counter.most_common(1)[0] if country_counter else None
+        total_top_country = self.total_stats["total_country_counter"].most_common(1)[0] if self.total_stats["total_country_counter"] else None
+        window_top_contestant = contestant_counter.most_common(1)[0] if contestant_counter else None
+        total_top_contestant = self.total_stats["total_contestant_counter"].most_common(1)[0] if self.total_stats["total_contestant_counter"] else None
 
         return {
-            "window_start":             self.ROLLING_WINDOW[0][0],
-            "window_end":               self.ROLLING_WINDOW[-1][0],
+            "window_start":             self.ROLLING_WINDOW[0][0].isoformat(),
+            "window_end":               self.ROLLING_WINDOW[-1][0].isoformat(),
 
             "window_posts":             window_posts,
             "total_posts":              self.total_stats["total_posts"],
@@ -73,17 +75,22 @@ class WindowAggregator:
             "total_avg_sentiment":      round(self.total_stats["total_sentiment"] / self.total_stats["total_posts"], 2),
 
             "window_platform_counter":  platform_counter,
-            "total_platform_counter":   self.total_stats["platform_counter"],
+            "total_platform_counter":   self.total_stats["total_platform_counter"],
             
             "window_top_song":          window_top_song,
             "window_top_country":       window_top_country,
             "window_top_contestant":    window_top_contestant,
             "total_top_song":           total_top_song,
             "total_top_country":        total_top_country,
-            "total_top_contestant":     total_top_contestant
+            "total_top_contestant":     total_top_contestant,
+            "event_label":              self.event_label
         }
 
     def add_post(self, post):
-        ts = datetime.fromisoformat(post["timestamp"].replace("Z", "+00:00"))
+        ts = post["timestamp"]
+        self.logger.info(f"Appending post: {post["post_id"]}")
         self.ROLLING_WINDOW.append((ts, post))
-        self.prune_window()
+        self.prune_window() 
+
+    def debug_clear_window(self):
+        self.ROLLING_WINDOW = deque()
