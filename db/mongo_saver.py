64cc85ca5dc673646b8cd717ad7b1c23f3e6f8d3
averelli <beloglazov.roman06@gmail.com>
2025-07-04 13:22:55 +0300

    style: clean up

--- db/mongo_saver.py
+++ db/mongo_saver.py
@@ -1,20 +1,19 @@
 from kafka import KafkaConsumer
 from config import settings
-from db.mongo import MongoDBClient
+from db.mongo_client import MongoDBClient
 import json
 
-consumer_raw = KafkaConsumer(
+consumer = KafkaConsumer(
     settings.KAFKA_PROCESSED_TOPIC,
     bootstrap_servers=settings.KAFKA_BOOTSTRAP_SERVERS,
-    auto_offset_reset='earliest',
-    group_id='test-router',
+    auto_offset_reset="earliest",
+    group_id="mongo_saver",
     enable_auto_commit=True
 )
 mongo = MongoDBClient("eurovison_streaming")
 
-for msg in consumer_raw:
+for msg in consumer:
     post = json.loads(msg.value)
-    print("Got a message:" + str(post["post_id"]))
     mongo.insert_post(post, "processed_posts")
     for clause in post["clauses"]:
         mongo.insert_post(clause, "processed_clauses")
